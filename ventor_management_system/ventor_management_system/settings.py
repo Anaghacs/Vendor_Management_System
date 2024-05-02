73dfbdf5e2dede7e3fc0a6417f0102b00b34e028
AnaghaCS <anaghasasidharan38@gmail.com>
2024-05-02 07:09:06 +0100

    create new app 'vendors'

--- ventor_management_system/ventor_management_system/settings.py
+++ ventor_management_system/ventor_management_system/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    #create new app 
+    "vendors"
+
+    #rest framework package installed
+    "rest_framework"
 ]
 
 MIDDLEWARE = [
